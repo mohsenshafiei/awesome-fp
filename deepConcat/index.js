2e40e44185689c3de41e510aa993a39a6462c414
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2019-12-10 22:54:33 +0800

    feat: add some test case.

--- deepConcat/index.js
+++ deepConcat/index.js
@@ -1,3 +1,4 @@
 const deepConcat = (arr, ...args) => args.reduce((res, arg) => Array.isArray(arg) ? [...res, ...deepConcat([], ...arg)] : [...res, arg], [...arr])
 console.log(deepConcat([1], 2, [3], [[4]]));
 console.log(deepConcat([1], 2, [3], [[[[[[[[[[[6]]]]]]]]]]], [[4]]));
+console.log(deepConcat([1], 2, [3], [[[[[[[[[[[[[[[[[[[[[[[[[[[4]]]]]]]]]]]]]]]]]]]]]]]]]]]));
